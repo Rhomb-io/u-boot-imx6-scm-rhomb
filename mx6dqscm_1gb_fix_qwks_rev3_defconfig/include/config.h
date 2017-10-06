b6534250cfa79565d285b6e6e913d659585f00ed
ppelaez <ppelaez@tecnofingers.com>
2017-10-06 13:18:38 +0200

    initial version

---
+++ mx6dqscm_1gb_fix_qwks_rev3_defconfig/include/config.h
@@ -0,0 +1,13 @@
+/* Automatically generated - do not edit */
+#define CONFIG_IMX_CONFIG	board/freescale/mx6dqscm/imximage_scm_lpddr2.cfg
+#define CONFIG_MX6Q	1
+#define CONFIG_DDR_MB	1024
+#define CONFIG_SYS_USE_SPINOR	1
+#define CONFIG_QWKS_REV3	1
+#define CONFIG_DEFAULT_FDT_FILE	"imx6dqscm-1gb-qwks-rev3-fix-ldo.dtb"
+#define CONFIG_BOARDDIR board/freescale/mx6dqscm
+#include <config_defaults.h>
+#include <config_uncmd_spl.h>
+#include <configs/mx6dqscm.h>
+#include <asm/config.h>
+#include <config_fallbacks.h>
---
+++ mx6dqscm_1gb_fix_qwks_rev3_defconfig/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,4 @@
+#define U_BOOT_DATE "Oct 05 2017"
+#define U_BOOT_TIME "19:34:43"
+#define U_BOOT_TZ "+0200"
+#define U_BOOT_DMI_DATE "10/05/2017"
